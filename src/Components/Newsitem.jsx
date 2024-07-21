863ac624bf304d2728b083113a3353c99189b697
unknown <tushar.ganvir21@vit.edu>
2024-07-22 01:48:01 +0530

    default image update

--- src/Components/Newsitem.jsx
+++ src/Components/Newsitem.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 export const Newsitem = ({ title, description, src, url }) => {
-  const defaultImage = '/src/assets/img/default-news.jpg';
+  const defaultImage = "./src/assets/img/default-news.jpg";
 
   return (
-    <div 
-      className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" 
+    <div
+      className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2"
       style={{
-        maxWidth: "345px", 
+        maxWidth: "345px",
         textAlign: "center",
         transition: "transform 0.3s ease-out, box-shadow 0.3s ease-out",
       }}
@@ -29,9 +29,13 @@ export const Newsitem = ({ title, description, src, url }) => {
       <div className="card-body">
         <h5 className="card-title">{title.slice(0, 50)}</h5>
         <p className="card-text">
-          {description ? description.slice(0, 90) : "Click the button below to read the full article and stay updated with the latest news."}
+          {description
+            ? description.slice(0, 90)
+            : "Click the button below to read the full article and stay updated with the latest news."}
         </p>
-        <a href={url} className="btn btn-primary">Read More</a>
+        <a href={url} className="btn btn-primary">
+          Read More
+        </a>
       </div>
     </div>
   );
