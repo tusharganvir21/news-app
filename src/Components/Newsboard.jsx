662ae3c6398bef2815eba71fc1b8b123902eb29c
unknown <tushar.ganvir21@vit.edu>
2024-08-07 23:15:23 +0530

    Update GitHub

--- src/Components/Newsboard.jsx
+++ src/Components/Newsboard.jsx
@@ -5,26 +5,12 @@ export const Newsboard = ({ category }) => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=27fd7377d7eb480f9fe5ee587f92644c`;
+    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }`;
     fetch(url)
       .then((response) => response.json())
-      .then((data) => {
-        console.log("Data received:", data);
-        if (!data || !data.articles) {
-          console.error("Invalid data structure:", data);
-          throw new Error("Invalid data structure");
-        }
-        // Check if articles is an array before accessing its length
-        if (Array.isArray(data.articles) && data.articles.length > 0) {
-          // Process articles
-          console.log("Articles:", data.articles);
-        } else {
-          console.log("No articles found or invalid articles structure");
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+      .then((data) => setArticles(data.articles));
   }, [category]);
 
   return (
